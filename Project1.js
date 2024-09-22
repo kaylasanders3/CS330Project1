3690ba801e8a1a8adf817e8188bcef0cb9bc3a22
kaylasanders3 <122752683+kaylasanders3@users.noreply.github.com>
2024-09-22 17:57:08 -0500

    Update Project1.js

--- Project1.js
+++ Project1.js
@@ -1,6 +1,6 @@
 function submitQuiz() {
 
-    var correctAnswers = 0; 
+    var correctAnswers = 0;
     var form = document.getElementById("quiz-form");
     var q1 = form["q1"].value;
     var q2 = form["q2"].value;
@@ -8,27 +8,33 @@ function submitQuiz() {
     var q4 = form["q4"].value;
     var q5 = form["q5"].value;
 
-    if(q1 === 'C') {
+    if (q1 === 'C') {
         correctAnswers++;
     }
-    if(q2 === 'A') {
+    if (q2 === 'A') {
         correctAnswers++;
     }
-    if(q3 === 'D') {
+    if (q3 === 'D') {
         correctAnswers++;
     }
-    if(q4 === 'A') {
+    if (q4 === 'A') {
         correctAnswers++;
     }
-    if(q5 === 'B') {
+    if (q5 === 'B') {
         correctAnswers++;
     }
 
-    if(correctAnswers <= 3) {
-        alert("You got " + correctAnswers + " out of 5 correct!");    
+    if (correctAnswers === 0) {
+        alert("You got " + correctAnswers + " out of 5 correct! Have you even seen the movie?");
+    }
+    else if (correctAnswers <= 2) {
+        alert("You got " + correctAnswers + " out of 5 correct! Better luck next time!");
+    }
+    else if (correctAnswers <= 4) {
+        alert("You got " + correctAnswers + " out of 5 correct! Not bad!");
     }
     else {
-        alert("You got " + correctAnswers + " out of 5 correct! Great job!");
+        alert("You got " + correctAnswers + " out of 5 correct! Congrats!");
     }
 };
 
